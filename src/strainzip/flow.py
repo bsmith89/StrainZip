2f8cf9500b375f9930463518d1429cf59566cfbe
Byron Smith <me@byronjsmith.com>
2024-03-25 12:15:26 -0700

    Try to only allocate edge/vertex property maps once in flow algorithm.

--- src/strainzip/flow.py
+++ src/strainzip/flow.py
@@ -7,19 +7,32 @@ import numpy as np
 def estimate_flow(graph, depth, weight, eps=0.001, maxiter=1000):
     target_vertex_weight = gt.edge_endpoint_property(graph, weight, "target")
     source_vertex_weight = gt.edge_endpoint_property(graph, weight, "source")
+
+    # Allocate PropertyMaps
     flow = graph.new_edge_property("float", val=1)
+    # In / Target
+    total_in_flow = graph.new_vertex_property("float")
+    in_flow_error = graph.new_vertex_property("float")
+    target_vertex_total_inflow = graph.new_edge_property("float")
+    target_vertex_total_inflow = graph.new_edge_property("float")
+    target_vertex_error = graph.new_edge_property("float")
+    # Out / Source
+    total_out_flow = graph.new_vertex_property("float")
+    out_flow_error = graph.new_vertex_property("float")
+    source_vertex_total_outflow = graph.new_edge_property("float")
+    source_vertex_total_outflow = graph.new_edge_property("float")
+    source_vertex_error = graph.new_edge_property("float")
+
     loss_hist = [np.finfo("float").max]
     i = 0
     for i in range(1, maxiter + 1):
-        # Inflow error
-        total_in_flow = gt.incident_edges_op(graph, "in", "sum", flow)
-        in_flow_error = graph.new_vertex_property(
-            "float", vals=depth.a - total_in_flow.a
+        # Update inflow error
+        gt.incident_edges_op(graph, "in", "sum", flow, total_in_flow)
+        in_flow_error.a[:] = depth.a - total_in_flow.a
+        gt.edge_endpoint_property(
+            graph, total_in_flow, "target", target_vertex_total_inflow
         )
-        target_vertex_total_inflow = gt.edge_endpoint_property(
-            graph, total_in_flow, "target"
-        )
-        target_vertex_error = gt.edge_endpoint_property(graph, in_flow_error, "target")
+        gt.edge_endpoint_property(graph, in_flow_error, "target", target_vertex_error)
         with np.errstate(divide="ignore", over="ignore", invalid="ignore"):
             target_vertex_alloc = np.nan_to_num(
                 flow.a / target_vertex_total_inflow.a, posinf=1, nan=0
@@ -27,14 +40,12 @@ def estimate_flow(graph, depth, weight, eps=0.001, maxiter=1000):
         target_vertex_alloc_error = target_vertex_alloc * target_vertex_error.a
 
         # Outflow error
-        total_out_flow = gt.incident_edges_op(graph, "out", "sum", flow)
-        out_flow_error = graph.new_vertex_property(
-            "float", vals=depth.a - total_out_flow.a
+        gt.incident_edges_op(graph, "out", "sum", flow, total_out_flow)
+        out_flow_error.a[:] = depth.a - total_out_flow.a
+        gt.edge_endpoint_property(
+            graph, total_out_flow, "source", source_vertex_total_outflow
         )
-        source_vertex_total_outflow = gt.edge_endpoint_property(
-            graph, total_out_flow, "source"
-        )
-        source_vertex_error = gt.edge_endpoint_property(graph, out_flow_error, "source")
+        gt.edge_endpoint_property(graph, out_flow_error, "source", source_vertex_error)
         with np.errstate(divide="ignore", over="ignore", invalid="ignore"):
             source_vertex_alloc = np.nan_to_num(
                 flow.a / source_vertex_total_outflow.a, posinf=1, nan=0
@@ -56,15 +67,11 @@ def estimate_flow(graph, depth, weight, eps=0.001, maxiter=1000):
             # are 0 because these two edge_properties include edge indices
             # for non-existent edges.
             # TODO: Consider running gt.reindex_edges to get rid of these.
-            mean_flow_error = graph.new_edge_property(
-                "float",
-                vals=(
-                    (source_vertex_alloc_error * source_vertex_weight.a)
-                    + (target_vertex_alloc_error * target_vertex_weight.a)
-                )
-                / (source_vertex_weight.a + target_vertex_weight.a),
-            )
-        flow = graph.new_edge_property("float", vals=flow.a + mean_flow_error.a)
+            mean_flow_error = (
+                (source_vertex_alloc_error * source_vertex_weight.a)
+                + (target_vertex_alloc_error * target_vertex_weight.a)
+            ) / (source_vertex_weight.a + target_vertex_weight.a)
+        flow = graph.new_edge_property("float", vals=flow.a + mean_flow_error)
     else:
         warn("Reached maxiter. Flow estimates did not converge.")
 
