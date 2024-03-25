c2ad4ee34da46dd3270cdd6ae73bee4038d3d3c6
Byron Smith <me@byronjsmith.com>
2024-03-25 10:07:03 -0700

    More thorough testing of flow estimation.

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,9 +1,11 @@
 import graph_tool as gt
+import numpy as np
+import pytest
 
 import strainzip as sz
 
 
-def test_simple_flow():
+def test_estimate_flow_convergence_warning():
     graph = gt.Graph()
     graph.add_edge_list([(0, 2), (1, 2), (2, 3), (2, 4)])
 
@@ -14,6 +16,65 @@ def test_simple_flow():
     depth.a[2] = 5
     depth.a[3] = 2
 
+    with pytest.warns(UserWarning):
+        flow = sz.flow.estimate_flow(
+            graph, depth=depth, weight=length, eps=0.001, maxiter=3
+        )
+
+
+def test_estimate_flow_cycle():
+    graph = gt.Graph()
+    graph.add_edge_list([(0, 1), (1, 2), (2, 3), (3, 0)])
+
+    length = graph.new_vertex_property("int", val=1)
+
+    depth = graph.new_vertex_property("float", val=1)
+    depth.a[0] = 2
+
     flow = sz.flow.estimate_flow(
         graph, depth=depth, weight=length, eps=0.001, maxiter=1000
     )
+    assert flow[(0, 1)] == 1.5
+    assert flow[(1, 2)] == 1.0
+    assert flow[(2, 3)] == 1.0
+    assert flow[(3, 0)] == 1.5
+
+
+def test_estimate_flow_minor_depth():
+    graph = gt.Graph()
+    graph.add_edge_list([(0, 1), (1, 2), (2, 3), (3, 0), (0, 4)])
+
+    length = graph.new_vertex_property("int", val=1)
+
+    depth = graph.new_vertex_property("float", val=1)
+    depth.a[4] = 1e-5
+
+    flow = sz.flow.estimate_flow(
+        graph, depth=depth, weight=length, eps=0.001, maxiter=1000
+    )
+    assert flow[(0, 1)] < 1.0
+    assert flow[(0, 4)] > 1e-5
+
+
+def test_estimate_flow_filtered_graph():
+    graph = gt.Graph()
+    graph.add_edge_list([(0, 1), (1, 2), (2, 3), (3, 0), (0, 4)])
+    filter = graph.new_vertex_property("bool", val=1)
+    graph.set_vertex_filter(filter)
+
+    length = graph.new_vertex_property("int", val=1)
+
+    depth = graph.new_vertex_property("float", val=1)
+    depth.a[4] = 1
+
+    flow = sz.flow.estimate_flow(
+        graph, depth=depth, weight=length, eps=0.001, maxiter=1000
+    )
+    assert flow[(0, 1)] < 1.0
+    assert flow[(0, 4)] < 1.0
+
+    filter.a[4] = 0
+    flow = sz.flow.estimate_flow(
+        graph, depth=depth, weight=length, eps=0.001, maxiter=1000
+    )
+    assert flow[(0, 1)] == 1.0
