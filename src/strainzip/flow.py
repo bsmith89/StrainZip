7b1366778c344aca10b99dc214bad764ca90d28c
Byron Smith <me@byronjsmith.com>
2024-03-25 14:19:38 -0700

    Do flow ops in-place as much as possible.

--- src/strainzip/flow.py
+++ src/strainzip/flow.py
@@ -14,47 +14,55 @@ def estimate_flow(graph, depth, weight, eps=0.001, maxiter=1000):
     total_in_flow = graph.new_vertex_property("float")
     in_flow_error = graph.new_vertex_property("float")
     target_vertex_total_inflow = graph.new_edge_property("float")
-    target_vertex_total_inflow = graph.new_edge_property("float")
     target_vertex_error = graph.new_edge_property("float")
+    target_vertex_alloc = np.empty_like(flow.a)
+    target_vertex_alloc_error = np.empty_like(flow.a)
     # Out / Source
     total_out_flow = graph.new_vertex_property("float")
     out_flow_error = graph.new_vertex_property("float")
     source_vertex_total_outflow = graph.new_edge_property("float")
-    source_vertex_total_outflow = graph.new_edge_property("float")
     source_vertex_error = graph.new_edge_property("float")
+    source_vertex_alloc = np.empty_like(flow.a)
+    source_vertex_alloc_error = np.empty_like(flow.a)
 
     loss_hist = [np.finfo("float").max]
     i = 0
     for i in range(1, maxiter + 1):
         # Update inflow error
         gt.incident_edges_op(graph, "in", "sum", flow, total_in_flow)
-        in_flow_error.a[:] = depth.a - total_in_flow.a
+        np.subtract(depth.a, total_in_flow.a, out=in_flow_error.a)
         gt.edge_endpoint_property(
             graph, total_in_flow, "target", target_vertex_total_inflow
         )
         gt.edge_endpoint_property(graph, in_flow_error, "target", target_vertex_error)
         with np.errstate(divide="ignore", over="ignore", invalid="ignore"):
+            np.divide(flow.a, target_vertex_total_inflow.a, out=target_vertex_alloc)
             target_vertex_alloc = np.nan_to_num(
-                flow.a / target_vertex_total_inflow.a, posinf=1, nan=0
+                flow.a / target_vertex_total_inflow.a, posinf=1, nan=0, copy=False
             )
-        target_vertex_alloc_error = target_vertex_alloc * target_vertex_error.a
+        np.multiply(
+            target_vertex_alloc, target_vertex_error.a, out=target_vertex_alloc_error
+        )
 
         # Outflow error
         gt.incident_edges_op(graph, "out", "sum", flow, total_out_flow)
-        out_flow_error.a[:] = depth.a - total_out_flow.a
+        np.subtract(depth.a, total_out_flow.a, out=out_flow_error.a)
         gt.edge_endpoint_property(
             graph, total_out_flow, "source", source_vertex_total_outflow
         )
         gt.edge_endpoint_property(graph, out_flow_error, "source", source_vertex_error)
         with np.errstate(divide="ignore", over="ignore", invalid="ignore"):
+            np.divide(flow.a, source_vertex_total_outflow.a, out=source_vertex_alloc)
             source_vertex_alloc = np.nan_to_num(
-                flow.a / source_vertex_total_outflow.a, posinf=1, nan=0
+                flow.a / source_vertex_total_outflow.a, posinf=1, nan=0, copy=False
             )
-        source_vertex_alloc_error = source_vertex_alloc * source_vertex_error.a
+        np.multiply(
+            source_vertex_alloc, source_vertex_error.a, out=source_vertex_alloc_error
+        )
 
         # Loss <- Sum of squared in/out-flow errors
         loss_hist.append(
-            np.square(in_flow_error.a).sum() + np.square(out_flow_error.a).sum()
+            float(np.square(in_flow_error.a).sum() + np.square(out_flow_error.a).sum())  # type: ignore[reportArgumentType]
         )
         if loss_hist[-1] == 0:
             break  # This should only happen if d is all 0's.
@@ -71,7 +79,7 @@ def estimate_flow(graph, depth, weight, eps=0.001, maxiter=1000):
                 (source_vertex_alloc_error * source_vertex_weight.a)
                 + (target_vertex_alloc_error * target_vertex_weight.a)
             ) / (source_vertex_weight.a + target_vertex_weight.a)
-        flow = graph.new_edge_property("float", vals=flow.a + mean_flow_error)
+        flow.a += mean_flow_error
     else:
         warn("Reached maxiter. Flow estimates did not converge.")
 
