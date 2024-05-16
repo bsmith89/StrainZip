4a5e504aecf0187085c8ebb5343fcb501f64a4ec
Byron Smith <me@byronjsmith.com>
2024-05-16 14:33:15 -0700

    Update SoftMaxNormal model to match updated/refactored model API.

--- src/strainzip/depth_model2.py
+++ src/strainzip/depth_model2.py
@@ -3,6 +3,7 @@ from typing import Any
 
 import jax.numpy as jnp
 import jaxopt
+import numpy as np
 from jax import hessian, jit
 from jax.nn import softplus
 from jax.scipy.stats.norm import logpdf as NormalLogPDF
@@ -15,7 +16,6 @@ from strainzip.errors import ConvergenceException
 class FitResult:
     beta: Any
     sigma: Any
-    score: float
     hessian_func: Any
     X: Any
     y: Any
@@ -23,20 +23,68 @@ class FitResult:
 
     @property
     def hessian_beta(self):
-        p_paths, s_samples = self.beta.shape
-        k_params = p_paths * s_samples
+        num_betas = self.num_paths * self.num_samples
         return self.hessian_func(self.beta, self.sigma)[0][0].reshape(
-            (k_params, k_params)
+            (num_betas, num_betas)
         )
 
     @property
+    def loglik(self):
+        return loglik(self.beta, self.sigma, self.y, self.X)
+
+    @property
     def covariance_beta(self):
-        cov = jnp.linalg.inv(self.hessian_beta)
+        try:
+            cov = jnp.linalg.inv(self.hessian_beta)
+        except np.linalg.LinAlgError:
+            cov = np.nan * np.ones_like(self.hessian_beta)
         return cov
 
     @property
+    def num_paths(self):
+        return self.X.shape[1]
+
+    @property
+    def num_params(self):
+        return self.num_paths * self.num_samples + self.num_samples
+
+    @property
+    def num_edges(self):
+        return self.y.shape[0]
+
+    @property
+    def num_samples(self):
+        return self.y.shape[1]
+
+    @property
+    def score(self):
+        return -self.bic
+
+    @property
+    def bic(self):
+        num_observations = self.num_edges * self.num_samples
+        bic = -2 * self.loglik + 2 * self.num_params * jnp.log(num_observations)
+        return bic
+
+    @property
+    def aic(self):
+        aic = -2 * self.loglik + 2 * self.num_params
+        return aic
+
+    @property
+    def aicc(self):
+        num_observations = self.num_edges * self.num_samples
+        aicc = self.aic + (2 * self.num_params**2 + 2 * self.num_params) / (
+            num_observations - self.num_params - 1
+        )
+        return aicc
+
+    @property
     def stderr_beta(self):
-        return jnp.sqrt(jnp.diag(self.covariance_beta)).reshape(self.beta.shape)
+        return np.nan_to_num(
+            jnp.sqrt(jnp.diag(self.covariance_beta)).reshape(self.beta.shape),
+            nan=np.inf,
+        )
 
     @property
     def residual(self):
@@ -52,60 +100,47 @@ def _negloglik(*args, **kwargs):
     return -1 * loglik(*args, **kwargs)
 
 
-def _score(beta, sigma, y, X):
-    e_edges, s_samples = y.shape
-    _, p_paths = X.shape
-
-    ll = loglik(beta, sigma, y, X)
-    n = e_edges * s_samples
-    k = p_paths * s_samples + s_samples
-    # NOTE: This parameter count is only correct if fitting both beta and sigma.
-    # However, it shouldn't matter for inter-comparison of this same depth model.
-    # bic = -2 * ll + 2 * k * jnp.log(n)  # TODO: Consier using BIC instead?
-    # aic = -2 * ll + 2 * k
-    bic = -2 * ll + 2 * k * jnp.log(n)
-    # aicc = aic + (2 * k**2 + 2 * k) / (n - k - 1)
-    # return -aicc  # Higher is better.
-    return -bic
+def _residual(beta, y, X):
+    expect = X @ beta
+    return y - expect
 
 
 @jit
 def _optimize(y, X, maxiter=500):
     e_edges, s_samples = y.shape
     e_edges, p_paths = X.shape
-    init_beta_raw = jnp.ones((p_paths, s_samples))
+    init_beta_raw = jnp.zeros((p_paths, s_samples))
 
     def objective(beta_raw, y, X):
-        expect = X @ softplus(beta_raw)
-        return ((y - expect) ** 2).sum()
+        beta = softplus(beta_raw)
+        return (_residual(beta, y, X) ** 2).sum()
 
     # Estimate beta by minimizing the sum of squared residuals.
-    beta_est_raw, opt = jaxopt.LBFGS(Partial(objective, y=y, X=X), maxiter=maxiter).run(
+    est_beta_raw, opt = jaxopt.LBFGS(Partial(objective, y=y, X=X), maxiter=maxiter).run(
         init_params=init_beta_raw,
     )
+    est_beta = softplus(est_beta_raw)
 
-    beta_est = softplus(beta_est_raw)
     # Estimate sigma as the root mean sum of squared residuals.
-    sigma_est = jnp.sqrt(((y - X @ beta_est) ** 2).mean(0, keepdims=True))
-    return (beta_est, sigma_est), opt
+    est_sigma = jnp.sqrt((_residual(est_beta, y, X) ** 2).mean(0, keepdims=True))
+    return (est_beta, est_sigma), opt
 
 
-def fit(y, X, *args, maxiter=500, **kwargs):
-    params_est, opt = _optimize(y=y, X=X, *args, maxiter=maxiter, **kwargs)
+def fit(y, X, maxiter=500):
+    params_est, opt = _optimize(y=y, X=X, maxiter=maxiter)
     if not opt.iter_num < maxiter:
         raise ConvergenceException(opt)
 
     # NOTE: Hessian of the *negative* log likelihood, because this is what's being
     # minimized? (How does this make sense??)
     hessian_func = hessian(
-        Partial(_negloglik, y=y, X=X, *args, **kwargs), argnums=range(len(params_est))
+        Partial(_negloglik, y=y, X=X), argnums=range(len(params_est))
     )
     # FIXME: Doesn't generalize well to other models. I'll have to change this line.
-    beta_est, sigma_est = params_est
+    est_beta, est_sigma = params_est
     fit_result = FitResult(
-        beta_est,
-        sigma_est,
-        _score(beta_est, sigma_est, y, X),
+        beta=est_beta,
+        sigma=est_sigma,
         hessian_func=hessian_func,
         X=X,
         y=y,
@@ -119,4 +154,4 @@ class SoftPlusNormal:
         self.maxiter = maxiter
 
     def fit(self, y, X):
-        return fit(y, X, maxiter=self.maxiter)
+        return fit(y, X, self.maxiter)
