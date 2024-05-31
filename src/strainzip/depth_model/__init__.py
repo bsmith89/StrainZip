64e6ae7ae57c9ed7776ce01160073123c5c34675
Byron Smith <me@byronjsmith.com>
2024-05-31 11:58:05 -0700

    Add a shared-variance Normal depth model.

--- src/strainzip/depth_model/__init__.py
+++ src/strainzip/depth_model/__init__.py
@@ -2,12 +2,14 @@ from typing import Any, Mapping
 
 from ._laplace import LaplaceDepthModel
 from ._normal import NormalDepthModel
+from ._normal_pooled import NormalPooledDepthModel
 from ._offset_log_normal import OffsetLogNormalDepthModel
 from ._studentst import StudentsTDepthModel
 
 NAMED_DEPTH_MODELS: Mapping[str, Any] = {
     "OffsetLogNormal": (OffsetLogNormalDepthModel, dict(alpha=1.0)),
     "Normal": (NormalDepthModel, dict()),
+    "NormalPooled": (NormalPooledDepthModel, dict()),
     "Laplace": (
         LaplaceDepthModel,
         dict(),
