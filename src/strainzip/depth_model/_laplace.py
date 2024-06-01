546715d95a7eb0903d551d89b533ec781a3c7f3b
Byron Smith <me@byronjsmith.com>
2024-05-31 17:04:12 -0700

    Update the not-working laplace model to be more obvious and less working.

--- src/strainzip/depth_model/_laplace.py
+++ src/strainzip/depth_model/_laplace.py
@@ -17,24 +17,25 @@ def _fit_laplace_model(y, X, maxiter=500):
     e_edges, s_samples = y.shape
     e_edges, p_paths = X.shape
     init_beta_raw = jnp.ones((p_paths, s_samples))
+    init_scale_raw = jnp.ones((1, s_samples))
 
-    def objective(beta_raw):
+    def objective(params_raw):
+        beta_raw, scale_raw = params_raw
         beta = softplus(beta_raw)
-        return (jnp.abs(_residual(beta, y, X))).sum()
+        scale = softplus(scale_raw)
+        return -laplace_logpdf(_residual(beta, y, X), loc=0, scale=scale).sum()
 
     # Estimate beta by minimizing the sum of squared residuals.
-    beta_est_raw, opt = jaxopt.LBFGS(objective, maxiter=maxiter).run(
-        init_params=init_beta_raw,
+    (beta_est_raw, scale_est_raw), opt = jaxopt.LBFGS(objective, maxiter=maxiter).run(
+        init_params=(init_beta_raw, init_scale_raw)
     )
     beta_est = softplus(beta_est_raw)
-    # Estimate scale as the mean of absolute residuals.
-    scale_est = (jnp.abs(_residual(beta_est, y, X))).mean(0, keepdims=True)
-    # NOTE: This has a separate sigma estimate for each sample.
-    return dict(beta=beta_est, sigma=scale_est), opt
+    scale_est = softplus(scale_est_raw)
+    return dict(beta=beta_est, scale=scale_est), opt
 
 
 class LaplaceDepthModel(JaxDepthModel):
-    param_names = ["sigma"]
+    param_names = ["scale"]
 
     def __init__(self, maxiter=500):
         self.maxiter = maxiter
@@ -51,4 +52,4 @@ class LaplaceDepthModel(JaxDepthModel):
 
     def _jax_loglik(self, beta, y, X, **params):
         expect = X @ beta
-        return laplace_logpdf(y, loc=expect, scale=params["sigma"]).sum()
+        return laplace_logpdf(y, loc=expect, scale=params["scale"]).sum()
