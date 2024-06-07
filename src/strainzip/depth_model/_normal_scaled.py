eb81af03c934ae1648c6e058c30a867bfc3d93cc
Byron Smith <me@byronjsmith.com>
2024-06-06 18:44:07 -0700

    Add a potential broken but philosophically interesting NormalScaled implementation.

--- src/strainzip/depth_model/_normal_scaled.py
+++ src/strainzip/depth_model/_normal_scaled.py
@@ -13,12 +13,7 @@ def weighted_least_squares(y, X, W):
     return beta
 
 
-def residual(beta, Y, X):
-    expect = X @ beta
-    resid = Y - expect
-    return resid
-
-
+# @jit
 def multi_sample_weighted_least_squares(Y, X, W):
     beta = []
     for i in range(Y.shape[1]):
@@ -43,49 +38,67 @@ def calculate_expected_var(beta, X, sigma, alpha):
     return expect_var
 
 
-def iterate_alternating_reweighted_least_squares(Y, X, alpha):
-    W_t = jnp.ones_like(Y)
-    while True:
-        beta_t = multi_sample_weighted_least_squares(Y, X, W=W_t)
-        sigma_t = estimate_sigma(beta_t, Y, X, alpha)
-        W_t = calculate_expected_var(beta_t, X, sigma_t, alpha)
-        yield beta_t, sigma_t
+def step_alternating_reweighted_least_squares(beta, sigma, Y, X, alpha):
+    W = calculate_expected_var(beta, X, sigma, alpha)
+    beta = multi_sample_weighted_least_squares(Y, X, W)
+    sigma = estimate_sigma(beta, Y, X, alpha)
+    return beta, sigma
 
 
+# def iterate_alternating_reweighted_least_squares(Y, X, alpha):
+#     W_t = jnp.ones_like(Y)
+#     while True:
+#         beta_t = multi_sample_weighted_least_squares(Y, X, W_t)
+#         sigma_t = estimate_sigma(beta_t, Y, X, alpha)
+#         W_t = calculate_expected_var(beta_t, X, sigma_t, alpha)
+#         yield beta_t, sigma_t
+#
+
+
+# @jit
 def loglik(beta, sigma, Y, X, alpha):
     expect = X @ beta
     return normal_logpdf(Y, loc=expect, scale=sigma * expect**alpha).sum()
+    # # FIXME: Debugging
+    # return normal_logpdf(Y, loc=expect, scale=sigma).sum()
 
 
-@jit
+loglik_grad_func = jit(grad(loglik, argnums=[0, 1]))
+
+
 def _fit_normal_scaled_model(y, X, alpha, maxiter, tol):
     e_edges, s_samples = y.shape
     e_edges, p_paths = X.shape
 
-    loglik_grad_func = grad(loglik)
-
     converged = False
     grad_norm = jnp.inf
-    beta_est = jnp.nan * jnp.ones((p_paths, s_samples))
-    sigma_est = jnp.nan
+    beta_est = jnp.ones((p_paths, s_samples))
+    sigma_est = jnp.ones((1, 1))
+    beta_grad = jnp.nan * beta_est
+    sigma_grad = jnp.nan * sigma_est
     i = -1
-    for i, (beta_est, sigma_est) in enumerate(
-        iterate_alternating_reweighted_least_squares(y, X, alpha)
-    ):
-        grad_norm = (loglik_grad_func(beta_est, sigma_est, y, X, alpha) ** 2).sum() ** (
-            1 / 2
+    for i in range(maxiter):
+        beta_est, sigma_est = step_alternating_reweighted_least_squares(
+            beta_est, sigma_est, y, X, alpha
         )
+        beta_grad, sigma_grad = loglik_grad_func(beta_est, sigma_est, y, X, alpha)
+        beta_grad_ss = (beta_grad**2).sum()
+        sigma_grad_ss = (sigma_grad**2).sum()
+        grad_norm = (beta_grad_ss + sigma_grad_ss) ** (1 / 2)
+
         if grad_norm == jnp.nan:
             break
         if grad_norm < tol:
             converged = True
             break
-        if i > maxiter:
-            break
 
-    return dict(beta=beta_est, sigma=sigma_est), dict(
-        converged=converged, grad_norm=grad_norm, iter_num=i
-    )
+    return dict(beta=beta_est, sigma=sigma_est), {
+        "converged": converged,
+        "grad_norm": grad_norm,
+        "beta_grad": beta_grad,
+        "sigma_grad": sigma_grad,
+        "iter_num": i,
+    }
 
 
 class NormalScaledDepthModel(JaxDepthModel):
